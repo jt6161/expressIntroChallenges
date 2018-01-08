fd2f456bf1dd74be6b7eeebcb24f3bb1d4cb15c7
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2018-01-08 16:10:24 -0700

    updates

--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
+var fs = require('fs')
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data) {})
+})
+
+app.post('/create/:name/:age', function(req, res) {
+  let newObj = {
+    name: req.params.name,
+    age: parseInt(req.params.age)
+  }
+  fs.readFile('./storage.json', 'utf8', function(err, data) {
+    let dataAsArr = JSON.parse(data);
+
+    dataAsArr.push(newObj);
+
+    fs.writeFile('./storage.json', JSON.stringify(dataAsArr), function(err) {
+      res.sendStatus(200);
+    })
+  })
 });
+
+
+
+
+
+
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
