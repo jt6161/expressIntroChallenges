72c32fa35d41808678cf8c9b79736c79aa39805a
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2018-01-08 14:37:00 -0700

    part1 completed

--- part1/server.js
+++ part1/server.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
 
-app.use(function(req, res) {
-  res.sendStatus(404);
+app.post('/create/:name', function(req, res) {
+  res.json({
+    id: 1,
+    name: req.params.name
+  });
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/verify/:age', function(req, res) {
+  let age = parseInt(req.params.age);
+  if (age > 13) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
 });
+
 
 app.listen(port, function() {
   console.log('Listening on port', port);
